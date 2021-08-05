c71039446c8909fb2f3879c4579fa7f0e8da752e
Varkal <moreau.romain83@gmail.com>
2021-08-05 22:16:58 +0200

    :wheel_of_dharma: the probe endpoint shouldn't required api token

--- src/key.guard.ts
+++ src/key.guard.ts
@@ -1,12 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class KeyGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     if (!process.env.API_KEY) {
+      return true;
+    }
+
+    if (this.reflector.get("public", context.getHandler()) === true) {
       return true;
     }
 
